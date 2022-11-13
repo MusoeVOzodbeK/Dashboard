c2bd116c8c9c91c85f8ab94858c950f520090768
musoev_ozodbek <musayev@gmail.com>
2022-11-13 13:38:04 +0500

    first commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,50 +1,52 @@
 import React, { createContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import {BsGlobe2} from 'react-icons/bs'
+import { BsGlobe2 } from "react-icons/bs";
 import "./navbar.css";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="nContainerLEft">
+      <div className="navbarContainer">
+        <div className="nContainerLEft">
           <button>
             <i className="bx bx-menu"></i>
           </button>
-        <ul>
-          <li>
-            <Link className="link" to="/">
-              Dashboard
+          <ul>
+            <li>
+              <Link className="link" to="/">
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/users">
+                Users
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/settings">
+                Settings
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="nContainerRight">
+          <button className="lang">
+            <BsGlobe2 className="nIcon" /> <p>English</p>
+          </button>
+          <button>
+            <i className="bx bx-bell"></i>
+          </button>
+          <button>
+            <i className="bx bx-list-ul"></i>
+          </button>
+          <button>
+            <i className="bx bx-message"></i>
+          </button>
+          <button>
+            <Link className="link" to="/login">
+              Login
             </Link>
-          </li>
-          <li>
-            <Link className="link" to="/users">
-              Users
-            </Link>
-          </li>
-          <li>
-            <Link className="link" to="/settings">
-              Settings
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className="nContainerRight">
-        <button className="lang">
-          <BsGlobe2 className="nIcon"/> <p>English</p>
-        </button>
-        <button>
-          <i className="bx bx-bell"></i>
-        </button>
-        <button>
-          <i className="bx bx-list-ul"></i>
-        </button>
-        <button>
-          <i className="bx bx-message"></i>
-        </button>
-        <button>
-          <Link className="link" to="/login">
-            Login
-          </Link>
-        </button>
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { BiGroup } from "react-icons/bi";
-import { MdOutlineGroups } from "react-icons/md";
+import { BiGroup, BiDollar, BiTransfer } from "react-icons/bi";
+import {
+  MdOutlineGroups,
+  MdOutlineMeetingRoom,
+  MdCastForEducation,
+  MdOutlineIntegrationInstructions,
+  MdOutlineFamilyRestroom,
+} from "react-icons/md";
 import { CiSettings } from "react-icons/ci";
-import { GrUserSettings, GrIntegration } from "react-icons/gr";
 import { SiGoogleclassroom } from "react-icons/si";
-import { FiMessageSquare } from "react-icons/fi";
+import { FiMessageSquare, FiUserCheck } from "react-icons/fi";
 import { AiOutlineUser, AiOutlineSetting } from "react-icons/ai";
-import { TbCurrencyDollarCanadian } from "react-icons/tb";
-import {GiProgression} from "react-icons/gi"
+import {
+  TbCurrencyDollarCanadian,
+  TbLayoutDashboard,
+  TbPresentationAnalytics,
+} from "react-icons/tb";
+import { GiProgression } from "react-icons/gi";
+import { BsBasket3, BsFillPersonLinesFill } from "react-icons/bs";
+import { HiOutlineAcademicCap, HiOutlineUserGroup } from "react-icons/hi";
+import { FaRegFileArchive } from "react-icons/fa";
+import { SlBadge } from "react-icons/sl";
 import "./sidebar.css";
 import logo from "../../assets/Group 1 (3).png";
 const Sidebar = () => {
@@ -26,18 +39,18 @@ const Sidebar = () => {
         </div>
         <ul className="sidebarList">
           <li className="sItem">
-            <Link className="link" to="/">
-              <i className="bx bx-home"></i> Dashboard
+            <Link className="link sListItem" to="/">
+              <TbLayoutDashboard className="sIcon" /> Dashboard
             </Link>
           </li>
           <li className="sItem">
-            <Link className="link" to="/marketing">
-           <GiProgression/>   Marketing
+            <Link className="link sListItem" to="/marketing">
+              <GiProgression className="sIcon" /> Marketing
             </Link>
           </li>
           <li className="sItem">
-            <li onClick={() => setOpenOne(!openOne)}>
-              <i className="bx bx-trending-up"></i> Analytics
+            <li onClick={() => setOpenOne(!openOne)} className="sListItem">
+              <TbPresentationAnalytics className="sIcon" /> Analytics
               <i
                 style={{
                   transform: openOne ? "rotate(180deg)" : "rotate(0deg)",
@@ -48,30 +61,33 @@ const Sidebar = () => {
             </li>
             <ul style={{ display: openOne ? "block" : "none" }}>
               <li>
-                <Link className="link" to="/analytics/finance">
-                  <TbCurrencyDollarCanadian /> Finance
+                <Link className="link sListItem" to="/analytics/finance">
+                  <TbCurrencyDollarCanadian className="sIcon" /> Finance
                 </Link>
               </li>
               <li className="sItem">
-                <Link className="link" to="/analytics/order">
-                  Order
+                <Link className="link sListItem" to="/analytics/order">
+                  <BsBasket3 className="sIcon" /> Order
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/analytics/allanalytics">
-                  All analytics
+                <Link className="link sListItem" to="/analytics/allanalytics">
+                  <TbPresentationAnalytics className="sIcon" /> All analytics
                 </Link>
               </li>
             </ul>
           </li>
           <li className="sItem">
-            <Link className="link" to="/order">
-              Order
+            <Link className="link sListItem" to="/order">
+              <BsBasket3 className="sIcon" /> Order
             </Link>
           </li>
           <li>
-            <li className="sItem" onClick={() => setOpenTwo(!openTwo)}>
-              <TbCurrencyDollarCanadian /> Finance
+            <li
+              className="sItem sListItem"
+              onClick={() => setOpenTwo(!openTwo)}
+            >
+              <TbCurrencyDollarCanadian className="sIcon" /> Finance
               <i
                 style={{
                   transform: openTwo ? "rotate(180deg)" : "rotate(0deg)",
@@ -82,30 +98,33 @@ const Sidebar = () => {
             </li>
             <ul style={{ display: openTwo ? "block" : "none" }}>
               <li>
-                <Link className="link" to="/finance/finance">
-                  <TbCurrencyDollarCanadian /> Finance
+                <Link className="link sListItem" to="/finance/finance">
+                  <TbCurrencyDollarCanadian className="sIcon" /> Finance
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/finance/typetransaction">
-                  Type Transaction
+                <Link className="link sListItem" to="/finance/typetransaction">
+                  <BiTransfer className="sIcon" /> Type Transaction
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/finance/prize">
-                  Prize
+                <Link className="link sListItem" to="/finance/prize">
+                  <SlBadge className="sIcon" /> Prize
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/finance/salary">
-                  <i className="bx bx-dollar"></i> Salary
+                <Link className="link sListItem" to="/finance/salary">
+                  <BiDollar className="sIcon" /> Salary
                 </Link>
               </li>
             </ul>
           </li>
           <li>
-            <li className="sItem" onClick={() => setOpenThree(!openThree)}>
-              Students
+            <li
+              className="sItem sListItem"
+              onClick={() => setOpenThree(!openThree)}
+            >
+              <HiOutlineAcademicCap className="sIcon" /> Students
               <i
                 style={{
                   transform: openThree ? "rotate(180deg)" : "rotate(0deg)",
@@ -121,30 +140,33 @@ const Sidebar = () => {
               }}
             >
               <li>
-                <Link className="link" to="/students/attendance">
-                  Attendance
+                <Link className="link sListItem" to="/students/attendance">
+                  <FiUserCheck className="sIcon" /> Attendance
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/students/allStudents">
-                  All Students
+                <Link className="link sListItem" to="/students/allStudents">
+                  <HiOutlineUserGroup className="sIcon" /> All Students
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/students/activestudents">
-                  Archive Students
+                <Link className="link sListItem" to="/students/activestudents">
+                  <FaRegFileArchive className="sIcon" /> Archive Students
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/students/parents">
-                  Parents
+                <Link className="link sListItem" to="/students/parents">
+                  <MdOutlineFamilyRestroom className="sIcon" /> Parents
                 </Link>
               </li>
             </ul>
           </li>
           <li>
-            <li className="sItem" onClick={() => setOpenFour(!openFour)}>
-              <MdOutlineGroups /> Groups
+            <li
+              className="sItem sListItem"
+              onClick={() => setOpenFour(!openFour)}
+            >
+              <MdOutlineGroups className="sIcon" /> Groups
               <i
                 style={{
                   transform: openFour ? "rotate(180deg)" : "rotate(0deg)",
@@ -155,35 +177,38 @@ const Sidebar = () => {
             </li>
             <ul style={{ display: openFour ? "block" : "none" }}>
               <li>
-                <Link className="link" to="/groups/groups">
-                  <MdOutlineGroups /> Groups
+                <Link className="link sListItem" to="/groups/groups">
+                  <MdOutlineGroups className="sIcon" /> Groups
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/groups/classschedule">
-                  <SiGoogleclassroom /> Class schedule
+                <Link className="link sListItem" to="/groups/classschedule">
+                  <SiGoogleclassroom className="sIcon" /> Class schedule
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/groups/rooms">
-                  Rooms
+                <Link className="link sListItem" to="/groups/rooms">
+                  <MdOutlineMeetingRoom className="sIcon" /> Rooms
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/groups/groupstudents">
-                  Group students
+                <Link className="link sListItem" to="/groups/groupstudents">
+                  <HiOutlineUserGroup className="sIcon" /> Group students
                 </Link>
               </li>
             </ul>
           </li>
           <li className="sItem">
-            <Link className="link" to="/courses">
-              Courses
+            <Link className="link sListItem" to="/courses">
+              <MdCastForEducation className="sIcon" /> Courses
             </Link>
           </li>
           <li>
-            <li className="sItem" onClick={() => setOpenFive(!openFive)}>
-              <GrUserSettings /> Employees
+            <li
+              className="sItem sListItem"
+              onClick={() => setOpenFive(!openFive)}
+            >
+              <BsFillPersonLinesFill className="sIcon" /> Employees
               <i
                 style={{
                   transform: openFive ? "rotate(180deg)" : "rotate(0deg)",
@@ -194,20 +219,23 @@ const Sidebar = () => {
             </li>
             <ul style={{ display: openFive ? "block" : "none" }}>
               <li>
-                <Link className="link" to="/employees/employees">
-                  Employees
+                <Link className="link sListItem" to="/employees/employees">
+                  <BsFillPersonLinesFill className="sIcon" /> Employees
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/employees/employeerroles">
-                  Employee roles
+                <Link className="link sListItem" to="/employees/employeerroles">
+                  <BsFillPersonLinesFill className="sIcon" /> Employee roles
                 </Link>
               </li>
             </ul>
           </li>
           <li>
-            <li className="sItem" onClick={() => setOpenSix(!openSix)}>
-              <AiOutlineSetting /> Settings
+            <li
+              className="sItem sListItem"
+              onClick={() => setOpenSix(!openSix)}
+            >
+              <AiOutlineSetting className="sIcon" /> Settings
               <i
                 style={{
                   transform: openSix ? "rotate(180deg)" : "rotate(0deg)",
@@ -218,33 +246,34 @@ const Sidebar = () => {
             </li>
             <ul style={{ display: openSix ? "block" : "none" }}>
               <li>
-                <Link className="link" to="/setting/allsettings">
-                  <CiSettings /> All Settings
+                <Link className="link sListItem" to="/setting/allsettings">
+                  <CiSettings className="sIcon" /> All Settings
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/setting/employess">
-                  <GrUserSettings /> Employees
+                <Link className="link sListItem" to="/setting/employess">
+                  <BsFillPersonLinesFill className="sIcon" /> Employees
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/setting/teachers">
-                  <AiOutlineUser /> Teachers
+                <Link className="link sListItem" to="/setting/teachers">
+                  <AiOutlineUser className="sIcon" /> Teachers
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/setting/students">
-                  <BiGroup /> Students
+                <Link className="link sListItem" to="/setting/students">
+                  <BiGroup className="sIcon" /> Students
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/setting/sms">
-                  <FiMessageSquare /> Sms
+                <Link className="link sListItem" to="/setting/sms">
+                  <FiMessageSquare className="sIcon" /> Sms
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/setting/integration">
-                  <GrIntegration /> Integration
+                <Link className="link sListItem" to="/setting/integration">
+                  <MdOutlineIntegrationInstructions className="sIcon" />{" "}
+                  Integration
                 </Link>
               </li>
             </ul>
